db899af43f3533692b76257750dd1d5e342386b0
JD037 <joj237@gmail.com>
2023-01-25 18:54:25 -0800

    3 variations of the same program

---
+++ uf_io_dem3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char f,m,l;
+	int age;
+
+	printf("Enter your initials, followed by your age: ");
+	scanf("%c %c %c %d", &f, &m, &l, &age);
+	printf("My initials are: %c%c%c and my age is %d.\n",'J','M','D',32);
+	return (0);
+}
